7fdcb21a3e1597376879d2390c29db9a789d4e21
Cristian <cristianwolfgang@gmail.com>
2022-09-16 12:47:55 -0400

    feat js prototypes

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert("HOTLINE MIAMI 2 \n Wrong Number");
+'use-strict';
+function message(type, styles){
+    this.type=type;
+    this.styles= "color:white;"+styles;
+}
+message.prototype.log = function(message){
+    console.log(`%c ${this.type}: ${message} %c`, `color:white;font-size:2rem;`+this.styles);
+}
+const errorMessage = new message("Error", "background-color:red");
+errorMessage.log("Not logged user");
